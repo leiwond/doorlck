1a894744dbf68edb248d2df03084dd20d70d9976
orangepi01 <foo@bar.com>
2018-03-26 14:59:43 +0000

    fix

--- start-all.sh
+++ start-all.sh
@@ -1,2 +1,2 @@
-nohup ./webserver-open.sh 19999 > /dev/null &
-nohup ./webserver.sh 8080 > /dev/null &
+nohup /home/bzero01/workspaces/testgpio/webserver-open.sh 19999 > /dev/null &
+nohup /home/bzero01/workspaces/testgpio/webserver.sh 8080 > /dev/null &
--- webserver-open.sh
+++ webserver-open.sh
@@ -2,6 +2,6 @@
 
 v="open..."
 RESPONSE="HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n${2:-"$v"}\r\n"
-while { python open.py; sleep 3; echo -en "$RESPONSE"; } | nc -q 1 -l "${1:-8080}"; do
+while { python /home/bzero01/workspaces/testgpio/open.py; sleep 3; echo -en "$RESPONSE"; } | nc -q 1 -l "${1:-8080}"; do
   echo "================================================"
 done
